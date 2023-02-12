b435fb84e71962bdedfce44c6f4a2bf9c573aaec
Olzhas Askar <olzhas.askar@gmail.com>
2023-02-12 22:37:41 +0000

    feat: indexing

--- week1/index_products.py
+++ week1/index_products.py
@@ -85,7 +85,18 @@ def get_opensearch():
     port = 9200
     auth = ('admin', 'admin')
     #### Step 2.a: Create a connection to OpenSearch
-    client = None
+    client = OpenSearch(
+        hosts=[{'host': host, 'port': port}],
+        http_compress=True,  # enables gzip compression for request bodies
+        http_auth=auth,
+        # client_cert = client_cert_path,
+        # client_key = client_key_path,
+        use_ssl=True,
+        verify_certs=False,
+        ssl_assert_hostname=False,
+        ssl_show_warn=False,
+        #ca_certs=ca_certs_path
+    )
     return client
 
 
@@ -107,8 +118,15 @@ def index_file(file, index_name):
         if 'productId' not in doc or len(doc['productId']) == 0:
             continue
         #### Step 2.b: Create a valid OpenSearch Doc and bulk index 2000 docs at a time
-        the_doc = None
+        the_doc = { '_index': index_name, '_source': doc }
         docs.append(the_doc)
+
+        if len(docs) > 2000:
+            bulk(client, docs, request_timeout=60)
+            docs = []
+    
+    if docs:
+        bulk(client, docs, request_timeout=60)
 
     return docs_indexed
 
