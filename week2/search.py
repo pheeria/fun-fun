77708d04b7edc02fa92c204aba0c40bdb4cdca45
Olzhas Askar <olzhas.askar@gmail.com>
2023-02-20 13:25:11 +0000

    feat: add autocomplete

--- week2/search.py
+++ week2/search.py
@@ -64,7 +64,21 @@ def autocomplete():
         if prefix is not None:
             type = request.args.get("type", "queries") # If type == queries, this is an autocomplete request, else if products, it's an instant search request.
             ##### W2, L3, S1
-            search_response = None
+            index = "bbuy_queries" if type == "queries" else "bbuy_products"
+            opensearch = get_opensearch()
+            search_response = opensearch.search(
+                index=index,
+                body={
+                    "suggest": {
+                        "autocomplete": {
+                            "prefix": prefix,
+                            "completion": {
+                                "field": "suggest",
+                                "skip_duplicates": True,
+                            }
+                        }
+                    }
+                })
             print("TODO: implement autocomplete AND instant search")
             if (search_response and search_response['suggest']['autocomplete'] and search_response['suggest']['autocomplete'][0]['length'] > 0): # just a query response
                 results = search_response['suggest']['autocomplete'][0]['options']
