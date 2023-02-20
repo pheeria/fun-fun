524d59ab979a3602a474d1b41dc821bfe3cb3f81
Olzhas Askar <olzhas.askar@gmail.com>
2023-02-20 09:45:01 +0000

    feat: suggesters

--- week2/search.py
+++ week2/search.py
@@ -106,7 +106,7 @@ def query():
 
         query_obj = qu.create_query(user_query,  [], sort, sortDir, size=20)  # We moved create_query to a utility class so we could use it elsewhere.
         ##### W2, L1, S2
-
+        qu.add_spelling_suggestions(query_obj, user_query)
         ##### W2, L2, S2
         print("Plain ol q: %s" % query_obj)
     elif request.method == 'GET':  # Handle the case where there is no query or just loading the page
